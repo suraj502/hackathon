2ad979cde1e29bd027d3ee395554a75a677de6ca
suraj502 <suraj502@users.noreply.github.com>
2026-07-09 20:12:06 +0530

    Enable Nitro for Vercel

--- dist/server/server.js
+++ dist/server/server.js
@@ -57,7 +57,7 @@ function renderErrorPage() {
 //#region src/server.ts
 var serverEntryPromise;
 async function getServerEntry() {
-	if (!serverEntryPromise) serverEntryPromise = import("./assets/server-jYsCDuqq.js").then((m) => m.default ?? m);
+	if (!serverEntryPromise) serverEntryPromise = import("./assets/server-B1LwY7ZI.js").then((m) => m.default ?? m);
 	return serverEntryPromise;
 }
 async function normalizeCatastrophicSsrResponse(response) {
